853c9eaa87c719e20f6b135a10797fe3ee616d66
kartik <kartikr@zignuts.com>
2023-07-19 16:38:54 +0530

    first commit

---
+++ react_assignment3/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Index from './component/Index';
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Index/>
+    </div>
+  );
+}
+
+export default App;
